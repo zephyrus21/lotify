c67676b67538785c6311979e01b7a382f14d16bc
zephyrus21 <pandey.piyus@gmail.com>
2021-09-14 18:24:43 +0530

    app int

---
+++ client/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Lotify</title>
+      </Head>
+
+      <main>
+        <h1>Lotify</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
