83c7cfa00ab3585db36ee289be95606feab8e984
zephyrus21 <pandey.piyus@gmail.com>
2021-10-05 18:18:07 +0530

    i fucked up

--- client/components/Dashboard.tsx
+++ client/components/Dashboard.tsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import SpotifyWebApi from "spotify-web-api-node";
+import SpotifyPlayer from "react-spotify-web-playback";
 import useAuth from "../hooks/useAuth";
 
 interface DashboardProps {
   code: string | string[];
 }
 
+const spotifyApi = new SpotifyWebApi({
+  clientId: "439a39d07c944a31920a230ffe3b5eed",
+});
+
 const Dashboard: React.FC<DashboardProps> = ({ code }) => {
   const token = useAuth(code);
-  return <h1>{code}</h1>;
+
+  useEffect(() => {
+    if (!token) return;
+    spotifyApi.setAccessToken(token);
+  }, [token]);
+
+  return (
+    <div>
+      <SpotifyPlayer
+        token={token as unknown as string}
+        uris={["spotify:artist:6HQYnRM4OzToCYPpVBInuU"]}
+      />
+      ;
+    </div>
+  );
 };
 
 export default Dashboard;
